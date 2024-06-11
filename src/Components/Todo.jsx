112bf2eb7f792852841e4f8e82ef426682cd6e5b
yasmineOueznadji <yasmineoueznadji0@gmail.com>
2024-06-11 17:45:30 +0200

    add Task

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,30 +1,58 @@
+import { useRef , useState} from "react";
+
 import todo_icon from "../assets/todo_icon.png";
 import ListItems from "./ListItems";
+
+
 const Todo = () => {
+  const inputRef = useRef();
+  const [todoList, setTodoList] = useState([]);
+
+  const addHandler = () => {
+    const inputTask = inputRef.current.value.trim();
+    if (!inputTask){
+        return null;
+    }
+    const taskData = {
+      id: Date.now(),
+      text: inputTask,
+      isComplete: false,
+    };
+
+    setTodoList((prevData) => [...prevData, taskData]);
+    inputRef.current.value = "";
+  };
+
   return (
     <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
-    {/*-----------Title------------*/} 
-    <div className="flex items-center mt-7 gap-2">
+      {/*-----------Title------------*/}
+      <div className="flex items-center mt-7 gap-2">
         <img className="w-8" src={todo_icon} alt="To-Do icon" />
         <h1 className="text-3xl font-semibold">To-Do List</h1>
       </div>
 
-      
-      {/*-----------Input Box------------*/} 
+      {/*-----------Input Box------------*/}
       <div className="flex items-center my-7 bg-gray-200 rounded-full">
         <input
+          ref={inputRef}
           className="bg-transparent border-0 outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-600"
           type="text"
           placeholder="Add Your Task"
         />
-        <button className="border-none rounded-full bg-orange-400 w-32 h-14 text-white text-lg font-medium cursor-pointer">
+        <button
+          onClick={addHandler}
+          className="border-none rounded-full bg-orange-400 w-32 h-14 text-white text-lg font-medium cursor-pointer"
+        >
           ADD Task
         </button>
       </div>
 
-      {/*-----------List Items------------*/} 
-<div><ListItems/></div>
-
+      {/*-----------List Items------------*/}
+      <div>
+        {todoList.map((item, idx) =>{
+            return <ListItems key={idx} text={item.text} />;
+        })}
+      </div>
     </div>
   );
 };
